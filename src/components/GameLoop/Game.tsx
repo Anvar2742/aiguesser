a9fd9b6b90580b775cdd72a2c9367c40fa66aa68
Anvar2742 <anvarmusa27@gmail.com>
2025-01-04 15:08:03 +0100

    assign roles

--- src/components/GameLoop/Game.tsx
+++ src/components/GameLoop/Game.tsx
@@ -1,17 +1,34 @@
-import { onPlayerJoin, insertCoin, isHost, myPlayer, Joystick, PlayerState } from "playroomkit";
+import { onPlayerJoin, insertCoin, PlayerState, useMultiplayerState } from "playroomkit";
 import { useEffect, useState } from "react";
 
 const Game = () => {
     const [players, setPlayers] = useState<PlayerState[]>([]);
+    const [status, setStatus] = useMultiplayerState('status', 0);
+
+    const assignRoles = (players: PlayerState[]) => {
+        if (players.length > 0) {
+            // Randomly select one player as the seeker (detective)
+            const seekerIndex = Math.floor(Math.random() * players.length);
+            players.forEach((player, index) => {
+                player.setState("role", index === seekerIndex ? "seeker" : "hider");
+            });
+        }
+    };
+
+    const onLaunch = () => {
+        setStatus(1, true)
+    };
 
     const start = async () => {
         // Start the game
-        await insertCoin();
+        await insertCoin({}, onLaunch);
 
         // Create a joystick controller for each joining player
         onPlayerJoin((state: PlayerState) => {
-            state.setState("letters", 0);
-            setPlayers((players) => [...players, state]);
+            setPlayers((players) => {
+                const updatedPlayers = [...players, state];
+                return updatedPlayers;
+            });
             state.onQuit(() => {
                 setPlayers((players) => players.filter((p) => p.id !== state.id));
             });
@@ -23,17 +40,21 @@ const Game = () => {
     }, []);
 
     useEffect(() => {
-        console.log(players);
+        if (players.length && status === 1) {
+            assignRoles(players)
+            setStatus(2, true)
+        }
     }, [players]);
 
+    if (status === 1) return
     return (
-        <>
+        <div className="h-screen bg-slate-900">
             {players.map((player) => (
                 <div key={player.id}>
-                    {player.getProfile().name}
+                    {player.getProfile().name} = {player.getState("role")}
                 </div>
             ))}
-        </>
+        </div>
     );
 };
 
